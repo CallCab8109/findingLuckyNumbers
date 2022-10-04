6407f229aa2316369f4831b1870a0a5e4e8c30b0
Carter Bleyl <Carter.Bleyl@gmail.com>
2022-10-04 10:37:17 -0600

    still need to delete repeats

---
+++ findLuckyNumbers.js
@@ -0,0 +1,17 @@
+// Write your code below this line.
+
+
+
+let arr = []
+
+function luckyNumbers(x) {
+    for(let i = 0; i < x; i++){
+    let possibleRoles = [1,2,3,4,5,6,7,8,9,10]
+    let rdmNum = Math.floor(Math.random() * possibleRoles.length)
+    }
+}
+ 
+let luckyNum = luckyNumbers(4)
+console.log(arr)
+
+
